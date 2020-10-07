d9e9b4c6359bf70442060552663ffb1dba510342
SuperSLD <37121925+SuperSLD@users.noreply.github.com>
2020-10-07 17:44:10 +0300

    СЛАУ метод простой прогонки

--- code/SimpleIteration.py
+++ code/SimpleIteration.py
@@ -3,18 +3,18 @@ import copy
 def simple_iteration(A, e):
     last_x = [0 for _ in range(len(A))]
     x = [1 for _ in range(len(A))]
-    while abs(x[0] - last_x[0]) > e:
-        x = [0 for _ in range(len(A))]
+    while True:
         for i in range(len(A)):
             x[i] = A[i][len(A[i]) - 1] / A[i][i]
             for j in range(len(A[i]) - 1):
                 if i != j:
                     x[i] += -last_x[j]*A[i][j]/A[i][i]
                     
-        print(last_x)
+        print("last_x: " + str(last_x))
+        if abs(x[0] - last_x[0]) <= e:
+            return last_x
         last_x = copy.copy(x)
-        
-    return last_x
+        x = [0 for _ in range(len(A))]
 
 A = [
     [22,  5, 10, -6,  45],
@@ -25,4 +25,4 @@ A = [
 
 e = 0.000001
 
-print(simple_iteration(A, e))
+print("решение: " + str(simple_iteration(A, e)))
